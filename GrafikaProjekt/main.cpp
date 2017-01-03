db0b3b50d1b8a0d085e2baa109f2ea1455e22962
persja40 <persja40@gmail.com>
2017-01-03 12:07:26 +0100

    Fullscreen, WindowHeight, WindowWidth

--- GrafikaProjekt/main.cpp
+++ GrafikaProjekt/main.cpp
@@ -1,20 +1,32 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window/Keyboard.hpp>
 #include "ProgramSettings.h"
+
+int WindowWidth;
+int WindowHeight;
+
 int main()
 {
 	ProgramSettings settings("settings.xml");
-
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	WindowWidth = sf::VideoMode::getDesktopMode().width;
+	WindowHeight = sf::VideoMode::getDesktopMode().height;
+	sf::RenderWindow window(sf::VideoMode(WindowWidth, WindowHeight), "SFML works!", sf::Style::Fullscreen);
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
 
-	while (window.isOpen())
-	{
+	while (window.isOpen()) {
 		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
+		while (window.pollEvent(event)) {
+			switch (event.type) {
+			case sf::Event::Closed:
 				window.close();
+				break;
+			case sf::Event::KeyPressed:
+				if (event.key.code == sf::Keyboard::Escape)
+					window.close();
+				break;
+			}
+
 		}
 
 		window.clear();
